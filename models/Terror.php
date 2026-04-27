4dfb60245d8669a04374ff406939755c637f1b7c
Alex Martínez <almama08@alumnatflorida.es>
2026-04-27 13:50:59 +0200

    Creación conf.json, subclases Accion y Terror, y clase Connection

---
+++ models/Terror.php
@@ -0,0 +1,18 @@
+<?php
+class Terror extends Videojuego{
+    private $tipoTerror;
+
+    function __construct($nombre,$duracion,$tipoTerror,$id=null){
+        parent::__construct($nombre,$duracion,$id);
+        $this->tipoTerror=$tipoTerror;
+    }
+
+    public function getTipoTerror(){
+        return $this->tipoTerror;
+    }
+
+    public function setTipoTerror($tipoTerror){
+        $this->tipoArmas = $tipoTerror;
+    }
+}
+?>
